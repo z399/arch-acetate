247bca991e6e9af3a8fd8fa196a7ce638a6f5c48
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-09-04 04:34:23 +0530

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -20,4 +20,8 @@ pacstrap /mnt base base-devel linux linux-firmware vim
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
+cp base-chroot.sh /mnt/home/base-chroot.sh
+
+chmod +x /mnt/home/base-chroot.sh
+
 arch-chroot /mnt 
