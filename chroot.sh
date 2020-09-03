f3dc78d0e0e8a2f1e7892896079547f775ee4b25
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-09-04 01:52:49 +0530

    Add files via upload

---
+++ chroot.sh
@@ -0,0 +1,34 @@
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+
+hwclock --systohc
+
+vim /etc/locale.gen
+
+locale-gen
+
+echo LANG=en_US.UTF-8 >> /etc/locale.conf 
+
+echo Arch Linux >> /etc/hostname
+
+passwd 
+
+pacman -S grub efibootmgr networkmanager
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+
+grub-mkconfig -o /boot/grub/grub.cfg 
+
+systemctl enable NetworkManager 
+
+useradd -mG wheel nebula
+
+passwd nebula
+
+EDITOR=vim visudo
+
+exit
+
+umount -R /mnt
+
+reboot
+
