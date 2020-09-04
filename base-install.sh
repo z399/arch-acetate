63fa1da3560c722a65a26b1adf02800e71630431
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-09-04 13:55:45 +0530

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -1,9 +1,5 @@
 pacman -Syy
 
-pacman -S reflector --noconfirm
-
-reflector --verbose --country 'India' -l 10 --sort rate --save /etc/pacman.d/mirrorlist
-
 ping -c 3 protonmail.com
 
 timedatectl set-ntp true
